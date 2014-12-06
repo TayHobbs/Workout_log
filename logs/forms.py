ce35b15e264b20d9675c5f44cb1cd3e572e83b92
Taylor Hobbs <taylor@feemon.com>
2014-12-06 14:45:32 -0600

    Style: Signup and login page css applied

--- logs/forms.py
+++ logs/forms.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
