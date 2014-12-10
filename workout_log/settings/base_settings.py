c37291e78e89692ef71b55bd1500cad8dd4e1489
Taylor Hobbs <taylor@feemon.com>
2014-12-09 21:21:07 -0600

    Feature: hijack manage.py to use the environment specified

---
+++ workout_log/settings/base_settings.py
@@ -0,0 +1,54 @@
+from os.path import abspath, join, dirname
+
+PROJECT_ROOT = abspath(join(dirname(__file__), '..'))
+ENV = None
+
+TEMPLATE_DEBUG = DEBUG = False
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+STATIC_URL = '/static/'
+SECRET_KEY = '!9oxr(8k34^z)&l#i-hvb0^i1#6x$zg2)(t4jbjvc99_ma4kor'
+STATICFILES_DIRS = (
+    abspath(join(PROJECT_ROOT, "..", "static")),
+)
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+ROOT_URLCONF = 'workout_log.urls'
+WSGI_APPLICATION = 'workout_log.wsgi.application'
+TEMPLATE_DIRS = (
+    abspath(join(PROJECT_ROOT, 'templates'))
+)
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+PROJECT_APPS = (
+    'logs',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+) + PROJECT_APPS
---
+++ workout_log/settings/dev.py
@@ -0,0 +1,16 @@
+from workout_log.settings.base_settings import *
+
+TEMPLATE_DEBUG = DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': abspath(join(PROJECT_ROOT, '..', 'db.sqlite3')),
+    }
+}
+
+INSTALLED_APPS += (
+    'django_nose',
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
