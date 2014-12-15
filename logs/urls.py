e691eb3cfabb9af96bde14409f1b254558c37e60
Taylor Hobbs <taylor@feemon.com>
2014-12-14 20:30:03 -0600

    Enhancement: Added logs to detail page url

--- logs/urls.py
+++ logs/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns("",
     url(r"^signup/$", views.signup, name="signup"),
     url(r"^login/$", views.user_login, name="login"),
     url(r"^logout/$", views.user_logout, name="logout"),
-    url("^(?P<log_id>\d+)/$", views.detail, name="detail"),
+    url("^logs/(?P<log_id>\d+)/$", views.detail, name="detail"),
     url("^create_new_log/$", views.create_new_log, name="create"),
     url("^add_to_log/$", views.add_to_log, name="add_to_log"),
 )
