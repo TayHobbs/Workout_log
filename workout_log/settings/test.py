20214a43f638979ba968df7f771f813e3c61ffe4
Taylor Hobbs <taylor@feemon.com>
2014-12-28 00:47:30 -0600

    Feature: added mysql to be database for production data

--- workout_log/settings/test.py
+++ workout_log/settings/test.py
@@ -4,12 +4,8 @@ TEMPLATE_DEBUG = DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'workout_logger',
-        'USER': 'taylor.hobbs',
-        'PASSWORD': 'asdf',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': abspath(join(PROJECT_ROOT, '..', 'db.sqlite3')),
     }
 }
 
